222f09d1365d1a903d35c6bb9b143d3abca84787
Zain <zain.ansari@live.com>
2022-02-18 17:19:22 +0000

    start node class

---
+++ index.js
@@ -0,0 +1,43 @@
+// The perceptron algorithm:
+// 1) For every input, multiply that input by its weight
+// 2) sum all of the weighted inputs
+// 3) Compute the output of the perceptron based on the 
+// sign of that sum passed through an activation function
+
+
+class Node {
+
+    weights = []
+
+    constructor(inputs){
+        this.inputs = inputs
+        for (let i = 0; i < inputs.length; i++) {
+            this.weights[i] = Math.random() - 0.5;
+          }
+    }
+
+    guess(){
+        let res = 0
+        for  (let i = 0; i < this.inputs.length; i++) {
+            res += this.inputs[i]*this.weights[i]
+        }
+        return this.sign(res)
+    }
+
+    // activation function
+     sign(n){
+         if(n < 0){
+             return -1
+         }
+         else{
+             return 1
+         }
+     }
+
+}
+
+
+ 
+inputs = [1, 2, 5]
+neuron = new Node(inputs)
+console.log(neuron.guess())
